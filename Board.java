c102ee5fcf32fe2323a80ec7ffd13f2bbe25a799
Jasper Burroughs <jasperjames8@icloud.com>
2023-09-28 19:48:34 -0400

    Finished getting the player inputted ships

--- Board.java
+++ Board.java
@@ -19,7 +19,7 @@ public class Board {
     public void printBoard() {
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
-                System.out.print(game[i][j]);
+                System.out.print(game[i][j] + " ");
             }
             System.out.println("");
         }
@@ -28,27 +28,27 @@ public class Board {
 
     public void setup() {
         for (int ship = 0; ship < ships.length; ship++) {
-            System.out.println("What is the first coordinate where you want to place your " + ships[ship] + " peg ship ([_], [_]): ");
+            System.out.println("What is the first coordinate where you want to place your " + ships[ship] + " peg ship (_, _): ");
             coord1 = scan.nextLine();
-            x1 = (int) (coord1.charAt(1)) - '1';
-            y1 = (int) (coord1.charAt(6)) - '1';
-            game[x1][y1] = '*';
+            y1 = (int) (coord1.charAt(0)) - '1';
+            x1 = (int) (coord1.charAt(3)) - '1';
+            game[y1][x1] = 1;
 
-            System.out.println("What is the last coordinate where you want to place your " + ships[ship] + " peg ship ([_], [_]): ");
+            System.out.println("What is the last coordinate where you want to place your " + ships[ship] + " peg ship (_, _): ");
             coord2 = scan.nextLine();
-            x2 = (int) (coord1.charAt(1)) - '1';
-            y2 = (int) (coord1.charAt(6)) - '1';
-            game[x2][y2] = '*';
+            y2 = (int) (coord2.charAt(0)) - '1';
+            x2 = (int) (coord2.charAt(3)) - '1';
+            game[y2][x2] = 1;
 
-            if (x1 == x2) {
-                for (int i = 0; i < y2 - y1; i++) {
-                    game[x1][i] = '*';
+            if (y1 == y2) {
+                for (int i = 0; i < x2 - x1; i++) {
+                    game[y1][i] = 1;
                 }
             }
-            else if (y1 == y2) {
+            else if (x1 == x2) {
                 direction = "col";
-                for (int i = 0; i < x2 - x1; i++) {
-                    game[i][y1] = '*';
+                for (int i = 0; i < y2 - y1; i++) {
+                    game[i][x1] = 1;
                 }
             }
     }
--- Tester.java
+++ Tester.java
@@ -1,10 +1,8 @@
 public class Tester {
     public static void main(String[] args) {
         Board b1 = new Board();
+        b1.setup();
         b1.printBoard();
-        b1.game[4][4] = 1;
-        b1.game[4][5] = 1;
-        System.out.println("");
-        b1.printBoard();
+
     }
 }
