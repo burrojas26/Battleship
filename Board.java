aa6ed1bd2024dd83c406d04b40b4aa205071b9da
Jasper Burroughs <burrojas26@7hills.org>
2023-10-04 12:32:23 -0400

    continued to add to the computer placement

--- Board.java
+++ Board.java
@@ -44,14 +44,14 @@ public class Board {
         //Iterates through all the ships for the player to place them
         for (int ship = 0; ship < ships.length; ship++) {
             overLap = true;
-            System.out.println("What is the first coordinate where you want to place your " + ships[ship] + " peg ship (_, _): ");
-            coord1 = scan.nextLine();
-            y1 = (int) (coord1.charAt(0)) - '1';
-            x1 = (int) (coord1.charAt(3)) - '1';
-            game[y1][x1] = 1;
-
             //Checks if the input has valid coordinates for the size of the ships
             while (overLap == true) {
+                System.out.println("What is the first coordinate where you want to place your " + ships[ship] + " peg ship (_, _): ");
+                coord1 = scan.nextLine();
+                y1 = (int) (coord1.charAt(0)) - '1';
+                x1 = (int) (coord1.charAt(3)) - '1';
+                game[y1][x1] = 1;
+
                 System.out.println("What is the last coordinate where you want to place your " + ships[ship] + " peg ship (_, _): ");
                 coord2 = scan.nextLine();
                 y2 = (int) (coord2.charAt(0)) - '1';
@@ -74,7 +74,7 @@ public class Board {
                         }
                     }
                     else {
-                        System.out.println("Incompatible coordinate, enter the last coordinate again");
+                        System.out.println("Incompatible coordinate, enter the coordinates again");
                     }
                     
                 }
@@ -113,18 +113,30 @@ public class Board {
                     y2 = y1;
                     x2 = x1+(int)(ships[ship]);
                     compGame[y2][x2] = 1;
+                    System.out.println("(" + x1 + ", " + y1 + ")");
+                    System.out.println("(" + x2 + ", " + y2 + ")");
                     for (int i = 0; i < x2 - x1; i++) {
-                        compGame[y1][i] = 1;
+                        //Check to make sure ships do not overlap
+                        if (game[y1][i+x1] == 0) {
+                            game[y1][i+x1] = 1;
+                            overLap = false;
+                        }
+                        else {
+                        overLap = true;
+                        }
                     }
                 }
                 else if (compDirection == 0) {
                     x2 = x1;
                     y2 = y1+(int)(ships[ship]);
                     compGame[y2][x2] = 1;
+                    System.out.println("(" + x1 + ", " + y1 + ")");
+                    System.out.println("(" + x2 + ", " + y2 + ")");
                     for (int i = 0; i < y2 - y1; i++) {
                         //Check to make sure ships do not overlap
-                        if (game[i][x1] == 0) {
-                            game[i][x1] = 1;
+                        if (game[i+y1][x1] == 0) {
+                            game[i+y1][x1] = 1;
+                            overLap = false;
                         }
                         else {
                         overLap = true;
