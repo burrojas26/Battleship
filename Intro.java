bc57377534d446cefed526506b793931afd7da27
Jasper Burroughs <burrojas26@7hills.org>
2023-10-11 13:12:54 -0400

    added a print statement

--- Intro.java
+++ Intro.java
@@ -4,6 +4,7 @@ public class Intro {
     Scanner scan = new Scanner(System.in);
     char option;
     
+    //Asks the user which mode they want to play, then returns the mode and prints instructions
     public String mode() {
         while (true) {
             System.out.println("Enter F for fast mode or N for normal mode: ");
@@ -13,17 +14,12 @@ public class Intro {
             }
         }
         if (option == 'F') {
-            return "fast";
-            System.out.println("Welcome to battleship, fast game. "); 
+            System.out.println("Welcome to battleship, fast game.");
             System.out.println("In this version the computer places your ships on an 8X8 grid for you. The computer also places ships on it's board. Your goal is to guess where all of the computer's ships are before they guess where all of your ships are");
+            return "fast";
         }
         else {
             return "norm";
         }
     }
-    
-    public void fastDirects() {
-        System.out.println
-    }
-
 }
