b5352d8199b5c541704b64f8ad935402d2f33ae0
Jasper Burroughs <jasperjames8@icloud.com>
2023-10-14 13:54:55 -0400

    added more directions

--- Intro.java
+++ Intro.java
@@ -17,14 +17,14 @@ public class Intro {
         if (option == 'F') {
             System.out.println("Welcome to battleship, fast game.");
             System.out.println("In this version the computer places your ships on an 8X8 grid for you. The computer also places ships on it's board. Your goal is to guess where all of the computer's ships are before they guess where all of your ships are.");
-            System.out.println("In your board a 0 represents empty water, while a number from 2 to 5 represents a ship of 2 size, a ship of 3 size, etc. If a 7 appears on your board that represents a part of a ship that has been hit.");
+            System.out.println("In your board a 0 represents empty water, while a number from 2 to 5 represents a ship of 2 size, a ship of 3 size, etc. If an X appears on your board that represents a part of a ship that has been hit. A # means that is a place in which the attacker has missed.");
             System.out.println("");
             return "fast";
         }
         else {
             System.out.println("Welcome to battleship, normal game.");
             System.out.println("In this version you place your ships on an 10X10 grid. The computer also places ships on it's board. Your goal is to guess where all of the computer's ships are before they guess where all of your ships are.");
-            System.out.println("In your board a 0 represents empty water, while a number from 2 to 5 represents a ship of 2 size, a ship of 3 size, etc. If a 7 appears on your board that represents a part of a ship that has been hit.");
+            System.out.println("In your board a 0 represents empty water, while a number from 2 to 5 represents a ship of 2 size, a ship of 3 size, etc. If an X appears on your board that represents a part of a ship that has been hit. A # means that is a place in which the attacker has missed.");
             System.out.println("");
             return "norm";
         }
